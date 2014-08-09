77edab82413825849af41cd1a7bf57712b5b8cbb
Ian Grant Jeffries <ian@housejeffries.com>
2014-08-09 03:38:36 +0200

    Touch up haskell stuff.

--- configuration.nix
+++ configuration.nix
@@ -70,6 +70,10 @@
     tarsnap
 
     # Basics for Haskell dev via cabal-install sandboxes
+    #
+    # This is the "Direct Install" strategy discussed here:
+    #
+    #     https://nixos.org/wiki/Haskell#Direct_installation
 
     haskellPackages.cabalInstall_1_18_0_3 # Earlier versions don't have `cabal sandbox`.
     haskellPlatform.ghc
