22da20c012dcc25074c8f6520059819225133a97
Ian Grant Jeffries <ian@housejeffries.com>
2014-08-11 02:35:21 +0200

    Create file for haskell dev.

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,6 @@
     }
   ];
 
-
   networking.hostName = "vivaine"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless.
 
@@ -39,6 +38,10 @@
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
+
+  require = [
+    ./sets/haskell-dev.nix
+  ];
 
   nixpkgs.config.allowUnfree = true;
 
@@ -68,36 +71,6 @@
 
     # Backups
     tarsnap
-
-    # Basics for Haskell dev via cabal-install sandboxes
-    #
-    # This is the "Direct Install" strategy discussed here:
-    #
-    #     https://nixos.org/wiki/Haskell#Direct_installation
-
-    haskellPackages.cabalInstall_1_18_0_3 # Earlier versions don't have `cabal sandbox`.
-    haskellPlatform.ghc
-    haskellPackages.yesodBin
-
-    # Other stuff for Haskell dev via cabal-install sandboxes
-
-    # Required for yesod or one of its dependencies via cabal-install.
-    haskellPackages.alex
-    # Required for language-javascript-0.5.13 which is needed for yesod.
-    haskellPackages.happy
-    # Required for yesod via cabal-install.
-    haskellPackages.zlib
-    # Maybe needed for `nix-shell` style haskell installs.
-    #
-    # Definitely needed for language-javascript-0.5.13 via cabal-install
-    # (which is needed for yesod). Otherwise you get "The program ar is
-    # required but it could not be found."
-    stdenv
-
-    # Other Haskell
-
-    haskellPackages.random
-    haskellPackages.cabal2nix
 
     # Other
 
