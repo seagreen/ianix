67576cf59f465cc6f9f0b591e16443a06533abac
Ian Grant Jeffries <ian@housejeffries.com>
2014-09-06 11:51:21 -0400

    Switch back to vim_configurable.

--- src/vim/c.nix
+++ src/vim/c.nix
@@ -2,5 +2,13 @@
 
 
 {
-  environment.systemPackages = with pkgs; [ vim ];
+  environment.systemPackages = with pkgs; [
+    # If I just use plain "vim" I get this error:
+    #
+    #     Error: jedi-vim requires vim compiled with +python
+    #
+    # Additionally pasting into vim via the middle mouse key
+    # may not work with the plain "vim" package.
+    vim_configurable
+  ];
 }
