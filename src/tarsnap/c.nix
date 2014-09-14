5ccd2e618fcdd0da5141f64d5b52fa27a5f7b0b7
Ian Grant Jeffries <ian@housejeffries.com>
2014-09-14 14:36:14 -0400

    Set regular backups.

--- src/tarsnap/c.nix
+++ src/tarsnap/c.nix
@@ -3,4 +3,9 @@
 
 {
   environment.systemPackages = with pkgs; [ tarsnap ];
+
+  services.cron.systemCronJobs = [
+    # Backup every day at 4 in the morning.
+    "0 4 * * * traveller /home/traveller/bin/backup"
+  ];
 }
