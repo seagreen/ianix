bef486df9841a738304a38e71eae0feb17d38de7
Ian Grant Jeffries <ian@housejeffries.com>
2014-10-03 11:49:58 -0400

    Improve comment.

--- src/mbsync/c.nix
+++ src/mbsync/c.nix
@@ -6,8 +6,11 @@
 
   services.cron.systemCronJobs = [
     # Run every 5 minutes.
+    #
     # The -a flag syncs all accounts.
-    # Note that we have to add ~/bin to PATH manually.
+    #
+    # Note that we have to add ~/bin to PATH manually. This is necessary
+    # because .mbsyncrc uses the pass-plain command, which is stored in ~/bin.
     "*/5 * * * * traveller export PATH=$PATH:~/bin; mbsync -a"
   ];
 }
