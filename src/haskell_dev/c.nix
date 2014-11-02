742368faa6f821e5e46b0ef75c8892dd6e322707
Ian Grant Jeffries <ian@housejeffries.com>
2014-11-01 23:57:59 -0400

    Finally complete 'cabal install' setup.

--- src/haskell_dev/c.nix
+++ src/haskell_dev/c.nix
@@ -1,27 +1,27 @@
 {config, pkgs, ... }:
 
-
+# Basics for Haskell dev via cabal-install sandboxes
+#
+# This is the "Direct Install" strategy discussed here:
+#
+#     https://nixos.org/wiki/Haskell#Direct_installation
 {
-  # Basics for Haskell dev via cabal-install sandboxes
-  #
-  # This is the "Direct Install" strategy discussed here:
-  #
-  #     https://nixos.org/wiki/Haskell#Direct_installation
   environment.systemPackages = with pkgs; [
 
     (haskellPackages_ghc783.ghcWithPackages (self : [
-      haskellPackages.zlib
     ]))
 
-    haskellPackages.cabalInstall_1_18_0_3 # Earlier versions don't have `cabal sandbox`.
+    # Earlier versions don't have `cabal sandbox`.
+    haskellPackages.cabalInstall_1_18_0_3 
 
-
-    haskellPackages.yesodBin
+    stdenv
 
     haskellPackages.alex
     haskellPackages.happy
-    # Maybe needed for `nix-shell` style haskell installs as well.
-    stdenv
+    haskellPackages.zlib
+
+    # For `yesod devel`.
+    haskellPackages.yesodBin
 
   ];
 }
