37617473ad9b9dbec2a80502983f0faa821473a7
Ian Grant Jeffries <ian@housejeffries.com>
2015-06-12 21:52:29 -0400

    Use latest cabal2nix to make escoger.

--- extended-nixpkgs/escoger/cabal2nix-output.nix
+++ extended-nixpkgs/escoger/cabal2nix-output.nix
@@ -2,16 +2,11 @@
 , parallel, QuickCheck, stdenv, test-framework
 , test-framework-hunit, test-framework-quickcheck2, unix, vector
 , vector-algorithms, vty
-,fetchFromGitHub }:
+}:
 mkDerivation {
   pname = "escoger";
   version = "0.1.0.0";
-  src = fetchFromGitHub {
-    owner = "tstat";
-    repo = "escoger";
-    rev = "b6b11c51846a9283ef4ee7c839b99ded9f6c3bc8";
-    sha256 = "0n6mvg5cm50ym20bz74b7q1afkljp0fc9pxhqk0ai82a71xxbxy3";
-  };
+  sha256 = "";
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
