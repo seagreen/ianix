9752a0b86371cb4953c406c9884351bcf890a542
Ian Grant Jeffries <ian@housejeffries.com>
2017-07-26 01:11:17 -0400

    Remove unused networking code.

--- src/networking/c.nix
+++ src/networking/c.nix
@@ -1,14 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    # Needed for nmapplet, but not nmcli.
-    networkmanagerapplet
-
-    # Fake system tray for nmapplet.
-    stalonetray 
-  ];
-
   networking.networkmanager.enable = true;
   users.extraGroups.networkmanager.members = [ "root" ];
 
